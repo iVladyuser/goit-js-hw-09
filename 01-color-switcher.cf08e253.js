1492ee58940044e73096d0b8a180b9615ba97037
iVladyuser <129949946+iVladyuser@users.noreply.github.com>
2023-08-31 14:02:04 +0000

    Deploying to gh-pages from @ iVladyuser/goit-js-hw-09@00d401ddccf3501491277e0545a03cf9f2446179 🚀

---
+++ 01-color-switcher.cf08e253.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){t.disabled=!0;var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.backgroundColor=e}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.cf08e253.js.map
